36e5704111471390854f0bf7ddef13404cfd74ee
Lucas <lucasmartiniscool@gmail.com>
2023-10-29 17:59:06 -0700

    Implemented data pointers

--- lab3.c
+++ lab3.c
@@ -5,30 +5,40 @@
 
 #define DEFAULT_ELEMENTS 8
 
-int N; // Size of the matrix/array
-int *array;
-bool stop = false; // Used to stop the simulations
+typedef struct {
+    int N; // Size of the matrix/array
+    int *array;
+    bool stop; // Used to stop the simulations
+    int *thread_ids;
+} Data;
+
+typedef struct {
+    Data *data;
+    int thread_id;
+} ThreadArgs;
+
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 // Functions shared for both problems
-void validate_input(int argc, char *argv[]) {
-    N = DEFAULT_ELEMENTS; // Default size
+void validate_input(int argc, char *argv[], void *arg) {
+    Data *data = (Data *)arg;
+    data->N = DEFAULT_ELEMENTS; // Default size
     if (argc > 1)
     {
         char *ptr;
         long input = strtol(argv[1], &ptr, 10);
         if (*ptr == '\0')
         {
-            N = (int)input;
+            data->N = (int)input;
         }
         else
         {
-            printf("Using Default Size of %d\n", N);
+            printf("Using Default Size of %d\n", data->N);
         }
     }
     else
     {
-        printf("Using Default Size of %d\n", N);
+        printf("Using Default Size of %d\n", data->N);
     }
 }
 
@@ -43,14 +53,16 @@ int display_menu() {
     return choice;
 }
 
-void create_threads(pthread_t *threads, int *thread_ids, int M, void *(*start_routine) (void *))
-{
-    for (int i = 0; i < M; i++)
-    {
-        thread_ids[i] = i;
-        pthread_create(&threads[i], NULL, start_routine, &thread_ids[i]);
+ThreadArgs* create_threads(pthread_t *threads, Data *data, int M, void *(*start_routine) (void *)) {
+    ThreadArgs* args = malloc(M * sizeof(ThreadArgs));
+    for (int i = 0; i < M; i++) {
+        args[i].data = data;
+        args[i].thread_id = i;
+        pthread_create(&threads[i], NULL, start_routine, &args[i]);
     }
+    return args;
 }
+
 
 void join_threads(pthread_t *threads, int M)
 {
@@ -61,45 +73,49 @@ void join_threads(pthread_t *threads, int M)
 }
 
 // Fill matrix for problem 1
-void fill_matrix()
+void fill_matrix(void *arg)
 {
-    for (int i = 0; i < N; i++)
+    Data *data = (Data *)arg;
+    for (int i = 0; i <  data->N; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j <  data->N; j++)
         {
-            array[i * N + j] = rand() % 2;
+            data->array[i *  data->N + j] = rand() % 2;
         }
     }
 }
 
 // Fill array for problem 2
-void fill_array()
+void fill_array(void *arg)
 {
-    for (int i = 0; i < N; i++)
+    Data *data = (Data *)arg;
+    for (int i = 0; i < data->N; i++)
     {
-        array[i] = rand() % 100;
+        data->array[i] = rand() % 100;
     }
 }
 
 // Print matrix for problem 1
-void print_matrix()
+void print_matrix(void *arg)
 {
-    for (int i = 0; i < N; i++)
+    Data *data = (Data *)arg;
+    for (int i = 0; i < data->N; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < data->N; j++)
         {
-            printf("%d ", array[i * N + j]);
+            printf("%d ", data->array[i * data->N + j]);
         }
         printf("\n");
     }
 }
 
 // Print array for problem 2
-void print_array()
+void print_array(void *arg)
 {
-    for (int i = 0; i < N; i++)
+    Data *data = (Data *)arg;
+    for (int i = 0; i < data->N; i++)
     {
-        printf("%d ", array[i]);
+        printf("%d ", data->array[i]);
     }
     printf("\n");
 }
@@ -107,29 +123,30 @@ void print_array()
 // Stop function for problem 1
 void *check_stop_criteria(void *arg)
 {
+    Data *data = (Data *)arg;
     while (1) // Keep running until stop is true
     {
         bool all_zero = true;
         bool all_one = true;
 
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < data->N; i++)
         {
-            for (int j = 0; j < N; j++)
+            for (int j = 0; j < data->N; j++)
             {
-                if (array[i * N + j] != 0)
+                if (data->array[i * data->N + j] != 0)
                 {
                     all_zero = false;
                 }
-                if (array[i * N + j] != 1)
+                if (data->array[i * data->N + j] != 1)
                 {
                     all_one = false;
                 }
             }
         }
 
-        stop = all_zero || all_one;
+        data->stop = all_zero || all_one;
 
-        if (stop) break; // Exit the loop if stop is true
+        if (data->stop) break; // Exit the loop if stop is true
     }
 
     return NULL;
@@ -138,20 +155,21 @@ void *check_stop_criteria(void *arg)
 // Stop function for problem 2
 void *check_sorted(void *arg)
 {
-    while (!stop)
+    Data *data = (Data *)arg;
+    while (!data->stop)
     {
         // Check if the array is sorted
         int sorted = 1;
-        for (int i = 0; i < N - 1; i++)
+        for (int i = 0; i < data->N - 1; i++)
         {
-            if (array[i] > array[i + 1])
+            if (data->array[i] > data->array[i + 1])
             {
                 sorted = 0;
             }
         }
         if (sorted)
         {
-            stop = true;
+            data->stop = true;
             pthread_exit(NULL);
             break;
         }
@@ -161,14 +179,16 @@ void *check_sorted(void *arg)
 
 void *flipNumbers(void *arg)
 {
-    int i, j;
-    int thread_id = *(int *)arg;
+    ThreadArgs *args = (ThreadArgs *)arg;
+    Data *data = args->data;
 
-    while (!stop) // Keep running until the stopping criteria is met
+    int i, j;
+
+    while (!data->stop) // Keep running until the stopping criteria is met
     {
         // Generate a column i and a row j randomly
-        i = rand() % N;
-        j = rand() % N;
+        i = rand() % data->N;
+        j = rand() % data->N;
 
         // Count the number of 0s and the total number of neighbors
         int count = 0;
@@ -179,9 +199,9 @@ void *flipNumbers(void *arg)
             {
                 if (x != 0 || y != 0) // Exclude the element itself
                 {
-                    if (i + x >= 0 && i + x < N && j + y >= 0 && j + y < N) // Check boundaries
+                    if (i + x >= 0 && i + x < data->N && j + y >= 0 && j + y < data->N) // Check boundaries
                     {
-                        if (array[(i + x) * N + j + y] == 0)
+                        if (data->array[(i + x) * data->N + j + y] == 0)
                         {
                             count++;
                         }
@@ -195,15 +215,15 @@ void *flipNumbers(void *arg)
         // Change the value of the matrix element based on the majority of its neighbors
         if (count > neighbors / 2)
         {
-            printf("Thread %d is changing the element at (%d, %d) to 0 from %d\n", thread_id, i, j, array[i * N + j]);
-            print_matrix();
-            array[i * N + j] = 0;
+            printf("Thread %d is changing the element at (%d, %d) to 0 from %d\n", args->thread_id, i, j, data->array[i * data->N + j]);
+            print_matrix(&data);
+            data->array[i * data->N + j] = 0;
         }
         else if (count < neighbors / 2)
         {
-            printf("Thread %d is changing the element at (%d, %d) to 1 from %d\n", thread_id, i, j, array[i * N + j]);
-            print_matrix();
-            array[i * N + j] = 1;
+            printf("Thread %d is changing the element at (%d, %d) to 1 from %d\n", args->thread_id, i, j, data->array[i * data->N + j]);
+            print_matrix(&data);
+            data->array[i * data->N + j] = 1;
         }
         pthread_mutex_unlock(&lock); // Unlock the mutex after changing the shared variable
     }
@@ -213,80 +233,90 @@ void *flipNumbers(void *arg)
 
 void *insertionSort(void *arg)
 {
-    int thread_id = *(int *)arg;
-    while (!stop)
+    ThreadArgs *args = (ThreadArgs *)arg;
+    Data *data = args->data;
+    while (! data->stop)
     {
         // Generate an index i randomly
-        int i = rand() % N;
+        int i = rand() %  data->N;
 
         pthread_mutex_lock(&lock); // Lock the mutex before accessing the shared variable
 
         // Insert array[i] into the sorted sequence array[0..i-1]
-        int key = array[i];
+        int key =  data->array[i];
         int j = i - 1;
-        while (j >= 0 && array[j] > key)
+        while (j >= 0 &&  data->array[j] > key)
         {
-            array[j + 1] = array[j];
+            data->array[j + 1] =  data->array[j];
             j = j - 1;
         }
-        array[j + 1] = key;
+        data->array[j + 1] = key;
 
-        printf("Thread %d has inserted %d at position %d in the array.\n", thread_id, key, j+1);
+        printf("Thread %d has inserted %d at position %d in the array.\n", args->thread_id, key, j+1);
 
         pthread_mutex_unlock(&lock); // Unlock the mutex after accessing the shared variable
     }
     return NULL;
 }
 
-void problem1(int M, pthread_t *threads, int *thread_ids, pthread_t stop_thread) {
-    array = malloc(N * N * sizeof(int));
+void problem1(int M, pthread_t *threads, pthread_t stop_thread, void *arg) {
+    Data *data = (Data *)arg;
+    data->array = malloc(data->N * data->N * sizeof(int));
 
     // Randomly fill the matrix with 0s and 1s
-    fill_matrix();
+    fill_matrix(data);
     printf("The starting matrix:\n");
-    print_matrix();
+    print_matrix(data);
 
     // Create a separate thread for checking the stopping criteria
-    pthread_create(&stop_thread, NULL, check_stop_criteria, NULL);
+    pthread_create(&stop_thread, NULL, check_stop_criteria, data);
 
     // Create M threads for problem 1
-    create_threads(threads, thread_ids, M, flipNumbers);
+    ThreadArgs* args = create_threads(threads, data, M, flipNumbers);
 
     // Join all threads
     join_threads(threads, M);
     pthread_join(stop_thread, NULL);
 
     printf("The ending matrix:\n");
-    print_matrix();
-    stop = false;
-    free(array);
+    print_matrix(data);
+    data->stop = false;
+    free(data->array);
+    free(args);
 }
 
-void problem2(int M, pthread_t *threads, int *thread_ids, pthread_t stop_thread) {
-    array = malloc(N * sizeof(int));
+void problem2(int M, pthread_t *threads, pthread_t stop_thread, void *arg) {
+    Data *data = (Data *)arg;
+    data->array = malloc(data->N * sizeof(int));
 
     // Randomly fill the array with numbers
-    fill_array();
+    fill_array(data);
     printf("Original Array: ");
-    print_array();
+    print_array(data);
+    printf("Original Array: ");
 
-    pthread_create(&stop_thread, NULL, check_sorted, NULL);
+    pthread_create(&stop_thread, NULL, check_sorted, data);
 
     // Create M threads for problem 2
-    create_threads(threads, thread_ids, M, insertionSort);
+    ThreadArgs* args = create_threads(threads, data, M, insertionSort);
 
     // Join all threads
     join_threads(threads, M);
     pthread_join(stop_thread, NULL);
 
     printf("Sorted Array: ");
-    print_array();
+    print_array(data);
 
-    free(array);
+    free(data->array);
+    free(args);
 }
 
 int main(int argc, char *argv[]) {
-    validate_input(argc, argv);
+    Data data;
+    data.N = DEFAULT_ELEMENTS; // Default size
+    data.stop = false;
+
+    validate_input(argc, argv, &data);
 
     int choice;
     do {
@@ -296,20 +326,18 @@ int main(int argc, char *argv[]) {
         printf("Enter the number of threads: ");
         scanf("%d", &M);
 
+        data.thread_ids = malloc(M * sizeof(int)); // Dynamically allocate memory for thread_ids
         pthread_t threads[M];
-        int *thread_ids = malloc(M * sizeof(int)); // Dynamically allocate memory for thread_ids
         pthread_t stop_thread;
 
         switch (choice) {
             case 1:
-                problem1(M, threads, thread_ids, stop_thread);
-                free(thread_ids); // Free the memory for thread_ids after all threads have finished executing
-                stop = false;
+                problem1(M, threads, stop_thread, &data);
+                data.stop = false;
                 break;
             case 2:
-                problem2(M, threads, thread_ids, stop_thread);
-                free(thread_ids); // Free the memory for thread_ids after all threads have finished executing
-                stop = false;
+                problem2(M, threads, stop_thread, &data);
+                data.stop = false;
                 break;
 
             case 3:
